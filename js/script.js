af8e905abb3bc9a93181150cea80ac896735dd9a
p77-web <paulfarcas13@gmail.com>
2019-04-13 21:53:54 -0400

    First commit website Maracana Canada

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let galerie = document.getElementById('galerie-header');
+let delegationPictures = document.getElementById('delegation-img');
+let header = document.getElementById('home-section');
+let leftCard = document.getElementById('left-card');
+let rightCard = document.getElementById('right-card');
+let leftCardTitle = document.getElementById('left-card-title');
+let rightCardTitle = document.getElementById('right-card-title');
+
+let fadePoint = 0;
+
+if (window.innerHeight < 600) {
+	fadePoint = 300;
+} else if (window.innerHeight < 900) {
+	fadePoint = 500;
+} else {
+	fadePoint = 700;
+}
+
+window.addEventListener('scroll', function() {
+	console.log('fadePoint', fadePoint);
+
+	console.log(
+		'leftCardTitle.scrollTop',
+		leftCardTitle.getBoundingClientRect().top
+	);
+	console.log('leftCardTitle', window.innerHeight);
+	// console.log('header', header.offsetHeight);
+
+	if (galerie.offsetTop <= window.scrollY + fadePoint) {
+		// console.log('Middle');
+
+		// delegationPictures.setAttribute(
+		// 	'style',
+		// 	'display: block; MozTransition: all 0.5s;'
+		// );
+
+		delegationPictures.style.opacity = '1';
+		delegationPictures.style.WebkitTransition = '2s';
+		delegationPictures.style.MozTransition = '2s';
+	}
+
+	if (leftCardTitle.getBoundingClientRect().top <= window.innerHeight - 200) {
+		leftCard.style.opacity = '1';
+		leftCard.style.WebkitTransition = '3s';
+		leftCard.style.MozTransition = '3s';
+	}
+
+	// if (leftCardTitle.getBoundingClientRect().top > window.innerHeight) {
+	// 	leftCard.style.opacity = '0';
+	// 	leftCard.style.WebkitTransition = '3s';
+	// 	leftCard.style.MozTransition = '3s';
+	// }
+
+	if (
+		rightCardTitle.getBoundingClientRect().top <=
+		window.innerHeight - 200
+	) {
+		rightCard.style.opacity = '1';
+		rightCard.style.WebkitTransition = '3s';
+		rightCard.style.MozTransition = '3s';
+	}
+
+	// if (rightCardTitle.getBoundingClientRect().top > window.innerHeight) {
+	// 	rightCard.style.opacity = '0';
+	// 	rightCard.style.WebkitTransition = '3s';
+	// 	rightCard.style.MozTransition = '3s';
+	// }
+});
---
+++ mail.php
@@ -0,0 +1,78 @@
+<?php
+$nom=addslashes(htmlentities($_POST['nom']));
+$courriel=addslashes(htmlentities($_POST['courriel']));
+$mess=addslashes(htmlentities($_POST['mess']));
+
+if (empty($_POST['nom']))
+echo '<body onLoad="alert(\'Merci de remplir le champ Nom\')">';
+if (empty($_POST['courriel']))
+echo '<body onLoad="alert(\'Merci de saisir votre courriel\')">';
+if (empty($_POST['mess']))
+echo '<body onLoad="alert(\'Vous ne pouvez pas envoyer un message vide\')">';
+
+//Je verifie que tous les champs sont renseignés 
+if ($nom != "" && $courriel != "" && $mess != "") {
+	$to = 'paulfarcas13@mgmail.com';
+	$subject = 'Formulaire en provenance du site internet';
+	$message = '
+		 <html>
+		  <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+		   <title>Maracana Canada</title>
+
+		  </head>
+		  <body>
+		  
+		   <table>
+		 
+			<tr>
+			 <td>*******************************************</td></tr>
+			 <tr><td> Indentifiants du message:</td>
+			</tr>
+			<tr>
+			 <td>Nom: '.$nom.'</td>
+			</tr>
+		   
+			<tr>
+			 <td>Courriel: '.$courriel.'</td>
+			</tr>
+		  
+			<tr>
+			 <td>message: '.$mess.'</td><tr>
+			</tr>
+			
+			<tr>
+			 <td>Adresse du Site : <a href="http://www.maracanacanada.com">www.maracanacanada.com</a></td>
+			</tr>
+		  
+		   <tr>
+			 <td>*******************************************</td>
+			</tr>
+		   
+		   </table>
+		  </body>
+		 </html>
+		 ';
+
+	$headers = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From: "Site internet de Maracana Canada" '."\r\n";
+	$headers .= "\r\n";
+
+	if (mail($to, $subject, $message, $headers)) // Envoi du message
+	{
+		echo '<body onLoad="alert(\'Votre message est transmis...\')">';
+	}
+	else // Non envoy�
+	{
+		echo "Votre message est non transmis";
+	}
+
+
+  
+} else {  
+  //j'invite l'utilisateur a remplir tous les champs
+	echo '<body onLoad="alert(\'merci de remplir toutes les cases...\')">';
+	echo '<meta http-equiv="refresh" content="0;URL=contact.html">';
+}
+echo '<meta http-equiv="refresh" content="0;URL=contact.html">';
+?>
