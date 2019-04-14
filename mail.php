e7a4aa966cc1e5841bdde729321e79ad2a5327b9
p77-web <paulfarcas13@gmail.com>
2019-04-14 12:59:59 -0400

    mail.php changed

--- mail.php
+++ mail.php
@@ -1,4 +1,6 @@
 <?php
+require("./sendgrid-php/sendgrid-php.php");
+
 $nom=addslashes(htmlentities($_POST['nom']));
 $courriel=addslashes(htmlentities($_POST['courriel']));
 $mess=addslashes(htmlentities($_POST['mess']));
@@ -10,68 +12,97 @@ echo '<body onLoad="alert(\'Merci de saisir votre courriel\')">';
 if (empty($_POST['mess']))
 echo '<body onLoad="alert(\'Vous ne pouvez pas envoyer un message vide\')">';
 
+$request_body = json_decode('{
+	"personalizations": [
+	  {
+		"to": [
+		  {
+			"email": "paulfarcas13@gmail.com"
+		  }
+		],
+		"subject": "Hello World from the SendGrid PHP Library!"
+	  }
+	],
+	"from": {
+	  "email": '.$courriel.'
+	},
+	"content": [
+	  {
+		"type": "text/plain",
+		"value": "Hello, Email!"
+	  }
+	]
+  }');
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
+echo $response->headers();
+
 //Je verifie que tous les champs sont renseignés 
 if ($nom != "" && $courriel != "" && $mess != "") {
-	$to = 'paulfarcas13@gmail.com';
-	$subject = 'Formulaire en provenance du site internet';
-	$message = '
-		 <html>
-		  <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
-		   <title>Maracana Canada</title>
+// 	$to = 'paulfarcas13@gmail.com';
+// 	$subject = 'Formulaire en provenance du site internet';
+// 	$message = '
+// 		 <html>
+// 		  <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+// 		   <title>Maracana Canada</title>
 
-		  </head>
-		  <body>
+// 		  </head>
+// 		  <body>
 		  
-		   <table>
+// 		   <table>
 		 
-			<tr>
-			 <td>*******************************************</td></tr>
-			 <tr><td> Indentifiants du message:</td>
-			</tr>
-			<tr>
-			 <td>Nom: '.$nom.'</td>
-			</tr>
+// 			<tr>
+// 			 <td>*******************************************</td></tr>
+// 			 <tr><td> Indentifiants du message:</td>
+// 			</tr>
+// 			<tr>
+// 			 <td>Nom: '.$nom.'</td>
+// 			</tr>
 		   
-			<tr>
-			 <td>Courriel: '.$courriel.'</td>
-			</tr>
+// 			<tr>
+// 			 <td>Courriel: '.$courriel.'</td>
+// 			</tr>
 		  
-			<tr>
-			 <td>message: '.$mess.'</td><tr>
-			</tr>
+// 			<tr>
+// 			 <td>message: '.$mess.'</td><tr>
+// 			</tr>
 			
-			<tr>
-			 <td>Adresse du Site : <a href="http://www.maracanacanada.com">www.maracanacanada.com</a></td>
-			</tr>
+// 			<tr>
+// 			 <td>Adresse du Site : <a href="http://www.maracanacanada.com">www.maracanacanada.com</a></td>
+// 			</tr>
 		  
-		   <tr>
-			 <td>*******************************************</td>
-			</tr>
+// 		   <tr>
+// 			 <td>*******************************************</td>
+// 			</tr>
 		   
-		   </table>
-		  </body>
-		 </html>
-		 ';
+// 		   </table>
+// 		  </body>
+// 		 </html>
+// 		 ';
 
-	$headers = 'MIME-Version: 1.0' . "\r\n";
-	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-	// $headers .= 'From: "Site internet de Maracana Canada" '."\r\n";
-	// $headers .= "\r\n";
+// 	$headers = 'MIME-Version: 1.0' . "\r\n";
+// 	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+// 	$headers .= 'From: "Site internet de Maracana Canada" '."\r\n";
+// 	$headers .= "\r\n";
 
-	if (mail($to, $subject, $message, $headers)) // Envoi du message
-	{
-		echo '<body onLoad="alert(\'Votre message est transmis...\')">';
-	}
-	else // Non envoy�
-	{
-		echo '<body onLoad="alert(\'Votre message est non transmis\')">';
-	}
+	// if (mail($to, $subject, $message, $headers)) 
+	// {
+	// 	echo '<body onLoad="alert(\'Votre message est transmis...\')">';
+	// }
+	// else 
+	// {
+	// 	echo "Votre message est non transmis";
+	// }
 
 
   
 } else {  
-  //j'invite l'utilisateur a remplir tous les champs
-	echo '<body onLoad="alert(\'merci de remplir toutes les cases...\')">';
+  	echo '<body onLoad="alert(\'merci de remplir toutes les cases...\')">';
 	echo '<meta http-equiv="refresh" content="0;URL=contact.html">';
 }
 echo '<meta http-equiv="refresh" content="0;URL=contact.html">';
