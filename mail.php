d4886156bb9e6edfcb8967fdb99ffaffc4fd408e
p77-web <paulfarcas13@gmail.com>
2019-04-14 16:14:58 -0400

    mail.php changed

--- mail.php
+++ mail.php
@@ -41,7 +41,7 @@ $request_body = json_decode('{
 	$sg = new \SendGrid(trim($apiKey));
 
 	$response = $sg->client->mail()->send()->post($request_body);
-	echo $request_body;
+	echo $response;
 	echo $response->statusCode();
 	echo $response->body();
 	echo $response->headers();
