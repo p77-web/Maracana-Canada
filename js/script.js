a56f0f763b9fb7761f41d23ef483f89ff3413de6
p77-web <paulfarcas13@gmail.com>
2019-04-22 02:10:41 -0400

    index.html

--- js/script.js
+++ js/script.js
@@ -18,53 +18,53 @@ if (window.innerHeight < 600) {
 
 console.log('testing');
 
-window.addEventListener('scroll', function() {
-	console.log('fadePoint', fadePoint);
+// window.addEventListener('scroll', function() {
+// 	console.log('fadePoint', fadePoint);
 
-	console.log(
-		'leftCardTitle.scrollTop',
-		leftCardTitle.getBoundingClientRect().top
-	);
-	console.log('leftCardTitle', window.innerHeight);
-	// console.log('header', header.offsetHeight);
+// 	console.log(
+// 		'leftCardTitle.scrollTop',
+// 		leftCardTitle.getBoundingClientRect().top
+// 	);
+// 	console.log('leftCardTitle', window.innerHeight);
+// 	// console.log('header', header.offsetHeight);
 
-	if (galerie.offsetTop <= window.scrollY + fadePoint) {
-		// console.log('Middle');
+// 	if (galerie.offsetTop <= window.scrollY + fadePoint) {
+// 		// console.log('Middle');
 
-		// delegationPictures.setAttribute(
-		// 	'style',
-		// 	'display: block; MozTransition: all 0.5s;'
-		// );
+// 		// delegationPictures.setAttribute(
+// 		// 	'style',
+// 		// 	'display: block; MozTransition: all 0.5s;'
+// 		// );
 
-		delegationPictures.style.opacity = '1';
-		delegationPictures.style.WebkitTransition = '2s';
-		delegationPictures.style.MozTransition = '2s';
-	}
+// 		delegationPictures.style.opacity = '1';
+// 		delegationPictures.style.WebkitTransition = '2s';
+// 		delegationPictures.style.MozTransition = '2s';
+// 	}
 
-	if (leftCardTitle.getBoundingClientRect().top <= window.innerHeight - 200) {
-		leftCard.style.opacity = '1';
-		leftCard.style.WebkitTransition = '3s';
-		leftCard.style.MozTransition = '3s';
-	}
+// 	if (leftCardTitle.getBoundingClientRect().top <= window.innerHeight - 200) {
+// 		leftCard.style.opacity = '1';
+// 		leftCard.style.WebkitTransition = '3s';
+// 		leftCard.style.MozTransition = '3s';
+// 	}
 
-	// if (leftCardTitle.getBoundingClientRect().top > window.innerHeight) {
-	// 	leftCard.style.opacity = '0';
-	// 	leftCard.style.WebkitTransition = '3s';
-	// 	leftCard.style.MozTransition = '3s';
-	// }
+// 	// if (leftCardTitle.getBoundingClientRect().top > window.innerHeight) {
+// 	// 	leftCard.style.opacity = '0';
+// 	// 	leftCard.style.WebkitTransition = '3s';
+// 	// 	leftCard.style.MozTransition = '3s';
+// 	// }
 
-	if (
-		rightCardTitle.getBoundingClientRect().top <=
-		window.innerHeight - 200
-	) {
-		rightCard.style.opacity = '1';
-		rightCard.style.WebkitTransition = '3s';
-		rightCard.style.MozTransition = '3s';
-	}
+// 	if (
+// 		rightCardTitle.getBoundingClientRect().top <=
+// 		window.innerHeight - 200
+// 	) {
+// 		rightCard.style.opacity = '1';
+// 		rightCard.style.WebkitTransition = '3s';
+// 		rightCard.style.MozTransition = '3s';
+// 	}
 
-	// if (rightCardTitle.getBoundingClientRect().top > window.innerHeight) {
-	// 	rightCard.style.opacity = '0';
-	// 	rightCard.style.WebkitTransition = '3s';
-	// 	rightCard.style.MozTransition = '3s';
-	// }
-});
+// 	// if (rightCardTitle.getBoundingClientRect().top > window.innerHeight) {
+// 	// 	rightCard.style.opacity = '0';
+// 	// 	rightCard.style.WebkitTransition = '3s';
+// 	// 	rightCard.style.MozTransition = '3s';
+// 	// }
+// });
