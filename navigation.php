59a77cf2cb839ff054a9bbb8590cb47e0e60b8c5
Paul Farcas <p77-web>
2022-07-28 00:42:43 -0400

    Php test added

---
+++ navigation.php
@@ -0,0 +1,105 @@
+<nav class="navbar navbar-expand-md navbar-dark navigation" id="main-nav">
+  <div class="container navbar__container">
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item mr-2">
+          <a href="index.html" class="nav-link translateText" data-key="home">
+            <span class="sr-only">(curent)</span>
+          </a>
+        </li>
+        <li class="nav-item mr-2">
+          <a href="apropos.html" class="nav-link translateText" data-key="about"></a>
+        </li>
+        <li class="nav-item mr-2 dropdown">
+          <a href="#" class="nav-link translateText nav-link--active" id="navbarDropdown-rules" role="button"
+            aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" data-key="rules"></a>
+          <div class="dropdown-content" aria-labelledby="navbarDropdown-rules">
+            <a href="" class="game-rules-nav-link translateText" target="_blank" data-key="laws"></a>
+            <a href="capsules.html" class="translateText" data-key="infosheet"></a>
+          </div>
+        </li>
+        <li class="nav-item mr-2">
+          <a href="conseiladmin.html" class="nav-link translateText" data-key="administration"></a>
+        </li>
+        <li class="nav-item mr-2 dropdown">
+          <a href="#" class="nav-link translateText nav-link--active" id="navbarDropdown-competitions"
+            role="button" aria-haspopup="true" aria-expanded="false" data-toggle="dropdown"
+            data-key="competitions"></a>
+          <ul class="navbar-nav mx-auto dropdown-content" aria-labelledby="navbarDropdown-competitions">
+            <li class="nav-item">
+              <a href="#" class="translateText" data-key="national"></a>
+            </li>
+            <li class="nav-item dropdown-content__submenu">
+              <a href="#" class="translateText dropdown-content__submenu-link" role="button"
+                aria-haspopup="true" aria-expanded="false" data-toggle="dropdown"
+                id="navbarDropdown-championship" data-key="championship"></a>
+              <ul class="navbar-nav dropdown-content dropdown-content__submenu-championship--position mx-auto"
+                aria-labelledby="navbarDropdown-championship">
+                <li class="nav-item">
+                  <a href="championship17.html" class="translateText dropdown-content__submenu-item"
+                    data-key="championship17"></a>
+                </li>
+                <li class="nav-item">
+                  <a href="championship18.html" class="translateText dropdown-content__submenu-item"
+                    data-key="championship18"></a>
+                </li>
+                <li class="nav-item">
+                  <a href="championship19.html" class="translateText dropdown-content__submenu-item"
+                    data-key="championship19"></a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown-content__submenu">
+              <a href="#" class="translateText dropdown-content__submenu-link" role="button"
+                aria-haspopup="true" aria-expanded="false" data-toggle="dropdown" id="navbarDropdown-cup"
+                data-key="cup"></a>
+              <ul class="navbar-nav dropdown-content dropdown-content__submenu-cup--position mx-auto"
+                aria-labelledby="navbarDropdown-cup">
+                <li class="nav-item">
+                  <a href="cup2017.html" class="translateText dropdown-content__submenu-item"
+                    data-key="cup17"></a>
+                </li>
+                <li class="nav-item">
+                  <a href="cup2018.html" class="translateText dropdown-content__submenu-item"
+                    data-key="cup18"></a>
+                </li>
+                <li class="nav-item">
+                  <a href="cup2019.html" class="translateText dropdown-content__submenu-item"
+                    data-key="cup19"></a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown-content__submenu">
+              <a href="competitions.html" class="translateText dropdown-content__submenu-link"
+                data-key="tournament"></a>
+            </li>
+            <li class="dropdown-divider"></li>
+            <li class="nav-item">
+              <a href="worldcups.html" class="translateText" data-key="international"></a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item mr-2">
+          <a href="clubs.html" class="nav-link translateText" data-key="clubs"></a>
+        </li>
+        <li class="nav-item mr-2">
+          <a href="contact.html" class="nav-link translateText" data-key="contact"></a>
+        </li>
+        <li class="nav-item mr-2">
+          <a href="http://www.facebook.com/maracanacanada" target="_blank" class="nav-link translateText">
+            <span>
+              <i class="fab fa-facebook-f"></i>
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="text-center" id="language">
+    <a class="translate translateText" id="languageSwitcher" data-key="buttonLanguage"
+      onClick="changeLanguage()"></a>
+  </div>
+</nav>
